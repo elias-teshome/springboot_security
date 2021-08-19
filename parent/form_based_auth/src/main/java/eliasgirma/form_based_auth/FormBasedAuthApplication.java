70d902e008dbaa2820b58f26da2fb5b0bc53cc84
Elias Girma <eliasgirma@Eliass-MacBook-Pro.local>
2021-08-19 06:28:18 -0500

    added parent with three module

--- parent/form_based_auth/src/main/java/eliasgirma/form_based_auth/FormBasedAuthApplication.java
+++ parent/form_based_auth/src/main/java/eliasgirma/form_based_auth/FormBasedAuthApplication.java
@@ -1,13 +1,13 @@
-package eliasgimra.springsesurity;
+package eliasgirma.form_based_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSesurityApplication {
+public class FormBasedAuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSesurityApplication.class, args);
+		SpringApplication.run(FormBasedAuthApplication.class, args);
 	}
 
 }
